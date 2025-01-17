55539f1e4b4835eac64defc88c7d76aa445cb207
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2025-01-17 20:57:50 +0200

    Add Basic Layout & Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import ProductList from './components/product-list/ProductList.tsx';
 
 function App() {
   return (
-    <div className="app">
-      <ProductList />
+    <div className="store">
+      <header className="store-header">
+        <div className="store-header-title">My Store</div>
+      </header>
+      <div className="store-products">
+        <div className="store-products-list">
+          <ProductList />
+        </div>
+        <div className="store-products-display"></div>
+      </div>
     </div>
   );
 }
--- src/components/product-list-item/ProductListItem.tsx
+++ src/components/product-list-item/ProductListItem.tsx
@@ -4,7 +4,14 @@ import { IProduct } from '../../models';
 
 const ProductListItem: React.FC<Partial<IProduct>> = ({ id, name, description = '', price, image }) => {
   return (
-    <div>{name}</div>
+    <div className="product">
+      <img src={image} alt={name} className="product-image" />
+      <div className="product-name">{name}</div>
+      <div className="product-description">{description}</div>
+      <div className="product-actions">
+        <button className="button delete">Delete</button>
+      </div>
+    </div>
   )
 }
 
